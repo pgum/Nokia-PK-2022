e3a89b6e504698b3b2e7545820504b39421d89f6
CZOLGOWUS <jkolodziej58@outlook.com>
2022-03-20 15:22:29 +0100

    add basic sunny|rainy day scenarios

--- UE/Tests/Application/ApplicationTestSuite.cpp
+++ UE/Tests/Application/ApplicationTestSuite.cpp
@@ -17,10 +17,13 @@ class ApplicationTestSuite : public Test
 {
 protected:
     const common::PhoneNumber PHONE_NUMBER{112};
+    const common::BtsId BTS_ID{42};
     NiceMock<common::ILoggerMock> loggerMock;
     StrictMock<IBtsPortMock> btsPortMock;
     StrictMock<IUserPortMock> userPortMock;
     StrictMock<ITimerPortMock> timerPortMock;
+
+    Expectation expectNoTConnected = EXPECT_CALL(userPortMock, showNotConnected());
 
     Application objectUnderTest{PHONE_NUMBER,
                                 loggerMock,
@@ -29,11 +32,74 @@ protected:
                                 timerPortMock};
 };
 
-struct ApplicationNotConnectedTestSuite : ApplicationTestSuite
-{};
 
-TEST_F(ApplicationNotConnectedTestSuite, todo)
+
+
+struct ApplicationNotConnectedTestSuite : ApplicationTestSuite
 {
+    void requestAttachOnSib();
+};
+
+void ApplicationNotConnectedTestSuite::requestAttachOnSib()
+{
+    EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+    EXPECT_CALL(timerPortMock, startTimer(_));
+    EXPECT_CALL(userPortMock,showConnecting());
+
+    objectUnderTest.handleSib(BTS_ID);
 }
+
+TEST_F(ApplicationNotConnectedTestSuite, shallRequestAttachOnSib)
+{
+    requestAttachOnSib();
+}
+
+
+
+
+struct ApplicationConnectingTestSuite : ApplicationNotConnectedTestSuite
+{
+    ApplicationConnectingTestSuite();
+    void requestConnectOnAttachRequest();
+};
+
+void ApplicationConnectingTestSuite::requestConnectOnAttachRequest()
+{
+    EXPECT_CALL(timerPortMock, stopTimer());
+    EXPECT_CALL(userPortMock,showConnected());
+
+    objectUnderTest.handleAttachAccept();
+}
+
+ApplicationConnectingTestSuite::ApplicationConnectingTestSuite()
+{
+    //preparation
+    requestAttachOnSib();
+}
+
+
+
+
+TEST_F(ApplicationConnectingTestSuite, shallCompleteAttachWhenAttachAccepted)
+{
+    requestConnectOnAttachRequest();
+}
+
+TEST_F(ApplicationConnectingTestSuite, shallFaillAttachWhenAttachRejected)
+{
+    EXPECT_CALL(timerPortMock, stopTimer());
+    EXPECT_CALL(userPortMock, showNotConnected());
+
+    objectUnderTest.handleAttachReject();
+}
+
+TEST_F(ApplicationConnectingTestSuite, shallFailAttachOnTimeOut)
+{
+    EXPECT_CALL(userPortMock, showNotConnected());
+
+    objectUnderTest.handleTimeout();
+}
+
+
 
 }
