1631435200fbef768f48af464f89fc095b41be7c
michalrudy9 <michallrudy9@gmail.com>
2022-04-09 16:48:34 +0000

    Added implementation for SMS DB

--- UE/Application/SMS/SMS_DB.cpp
+++ UE/Application/SMS/SMS_DB.cpp
@@ -1,2 +1,16 @@
+#include "SMS_DB.hpp"
 
+SMS_DB::SMS_DB(){};
+
+SMS_DB::~SMS_DB(){};
+
+void SMS_DB::addSmsToDB(SMS sms)
+{
+    this->m_smsList.push_back(sms);
+}
+
+void SMS_DB::getAllSMS()
+{
+    //TODO
+}
 
--- UE/Application/SMS/SMS_DB.hpp
+++ UE/Application/SMS/SMS_DB.hpp
@@ -6,8 +6,10 @@ class SMS_DB{
     std::vector<SMS> m_smsList;
 
 public:
+    SMS_DB();
     void addSmsToDB(SMS sms);
 
     void getAllSMS();
 
+    virtual ~SMS_DB();
 };
