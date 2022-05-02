406bc68b1dc6d41df4d9e3e0e0153f4199065c34
michalrudy9 <michallrudy9@gmail.com>
2022-05-02 15:47:51 +0000

    ConnectedState modification for moving in the menu functionality

--- UE/Application/States/ConnectedState.cpp
+++ UE/Application/States/ConnectedState.cpp
@@ -1,5 +1,6 @@
 #include "ConnectedState.hpp"
 #include "NotConnectedState.hpp"
+#include "ViewingSmsState.hpp"
 
 namespace ue
 {
@@ -7,7 +8,28 @@ namespace ue
     ConnectedState::ConnectedState(Context &context)
         : BaseState(context, "ConnectedState")
     {
+        context.user.acceptCallback([this] { onAcceptCallbackClicked(); });
+        context.user.rejectCallback([this] { onDeclineCallbackClicked(); });
         context.user.showConnected();
+    }
+
+    void ConnectedState::onAcceptCallbackClicked()
+    {
+        int currentMenuIndex = context.user.getMenuIndex();
+
+        if (currentMenuIndex == 1)
+        {
+            context.logger.logInfo("Konrad Was");
+        }
+        if (currentMenuIndex == 2)
+        {
+            context.setState<ViewingSmsState>();
+        }
+    }
+
+    void ConnectedState::onDeclineCallbackClicked()
+    {
+        context.logger.logInfo("Jakub Pietrzko");
     }
 
     void ConnectedState::handleDisconnected()
