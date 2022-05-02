550357e51d7faa238b4af7df70a8b1ae81fc3c1f
konrad <konrad.was@student.pk.edu.pl>
2022-05-02 08:40:50 -0700

    SMS/SMS_DB modification for navigating in the menu functionality

--- UE/Application/SMS/SMS_DB.cpp
+++ UE/Application/SMS/SMS_DB.cpp
@@ -1,16 +1,14 @@
 #include "SMS_DB.hpp"
 
-SMS_DB::SMS_DB(){};
+SMS_DB::SMS_DB() {};
 
-SMS_DB::~SMS_DB(){};
+SMS_DB::~SMS_DB() {};
 
-void SMS_DB::addSmsToDB(SMS sms)
-{
+void SMS_DB::addSmsToDB(SMS sms) {
     this->m_smsList.push_back(sms);
 }
 
-void SMS_DB::getAllSMS()
-{
-    //TODO
+std::vector<SMS> SMS_DB::getAllSMS() {
+    return m_smsList;
 }
 
--- UE/Application/SMS/SMS_DB.hpp
+++ UE/Application/SMS/SMS_DB.hpp
@@ -1,15 +1,17 @@
 #include "SMS.hpp"
 #include <vector>
+
 #pragma once
 
-class SMS_DB{
+class SMS_DB {
     std::vector<SMS> m_smsList;
 
 public:
     SMS_DB();
+
     void addSmsToDB(SMS sms);
 
-    void getAllSMS();
+    std::vector<SMS> getAllSMS();
 
     virtual ~SMS_DB();
 };
