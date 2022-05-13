276df28358e91ec6adbcf40d8a1b9289f9889481
CKTA00 <przekozuch@gmail.com>
2022-05-13 20:18:37 +0200

    Fixed: ITextMessage was not part of ue namespace

--- UE/Application/SMSDB/ITextMessege.hpp
+++ UE/Application/SMSDB/ITextMessege.hpp
@@ -2,11 +2,16 @@
 
 #include "Messages/PhoneNumber.hpp"
 
-class ITextMessage
-{
+namespace ue {
+
+class ITextMessage {
 public:
-    virtual std::string getMessageSummary() = 0;        // let summary be less than 16 characters long (for optimization)
+    virtual std::string
+    getMessageSummary() = 0;        // let summary be less than 16 characters long (for optimization)
     virtual std::string getMessage() = 0;               // get full message
     virtual common::PhoneNumber getFromNumber() = 0;
+
     virtual common::PhoneNumber getToNumber() = 0;
 };
+
+}
