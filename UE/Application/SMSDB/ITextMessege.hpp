473f101b6c386c2cc794e23cf0d280306ad67d05
CKTA00 <przekozuch@gmail.com>
2022-05-15 19:41:21 +0200

    Fix formatting

--- UE/Application/SMSDB/ITextMessege.hpp
+++ UE/Application/SMSDB/ITextMessege.hpp
@@ -14,8 +14,7 @@ namespace ue {
 
 class ITextMessage {
 public:
-    virtual std::string
-    getMessageSummary() = 0;        // let summary be less than 16 characters long (for optimization)
+    virtual std::string getMessageSummary() = 0;        // let summary be less than 16 characters long (for optimization)
     virtual std::string getMessage() = 0;               // get full message
     virtual common::PhoneNumber getFromNumber() = 0;
 
